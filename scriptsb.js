fdce0be48edea28559a094f38f650278e8feb4d0
thieve <thieve@users.noreply.github.com>
2017-02-17 06:18:11 -0500

    Add files via upload

--- scriptsb.js
+++ scriptsb.js
@@ -31,56 +31,20 @@ $(document).ready(function(){
 		console.log("shift!")
 	});
 
-window.addEventListener("keydown", (function(canMove) {
-  return function(event) {
-    if (!canMove) return false;
-    canMove = false;
-    setTimeout(function() { canMove = true; }, 400);
-    switch (event.keyCode) {
-		case 37:
-			var x = (1936) - $("#menu").scrollLeft();
-			$("#menu").animate({ scrollLeft: -x })
-			break;
-    	case 39:
-			var x = (1936) + $("#menu").scrollLeft();
-			$("#menu").animate({ scrollLeft: +x })
-			console.log("new test!")
-			break;
-    	case 38:
-			$("#container").toggleClass("active");
-			$("#games").fadeToggle(300);
-			$("#games").toggleClass("active");
-			break;
-		case 40:
-			$("#container").removeClass("active");
-			$("#games").fadeOut(300);
-			$("#games").removeClass("active");
-			break;		
-    }  
-  };
-})(true), false);
-
-
-
-/*
-	document.onkeydown = function(e) {
-		console.log("keypressed!");
-		var box = $("#menu");
-		var canMove = false;
-		setTimeout(function() { canMove = true; }, 250);
-		switch (e.keyCode) {
+	window.addEventListener("keydown", (function(canMove) {
+	return function(event) {
+		if (!canMove) return false;
+		canMove = false;
+		setTimeout(function() { canMove = true; }, 400);
+		switch (event.keyCode) {
 			case 37:
-				console.log("left!");
-				var x = (1936) - box.scrollLeft();
-				box.animate({ scrollLeft: -x })
+				var x = (1936) - $("#menu").scrollLeft();
+				$("#menu").animate({ scrollLeft: -x })
 				break;
 			case 39:
-				console.log("right!");
-				clearTimeout(scrolling);
-				scrolling = setTimeout(function(){
-					var x = (1936) + box.scrollLeft();
-					box.animate({ scrollLeft: x })
-				}, x);
+				var x = (1936) + $("#menu").scrollLeft();
+				$("#menu").animate({ scrollLeft: +x });
+				console.log("new test!");
 				break;
 			case 38:
 				$("#container").toggleClass("active");
@@ -91,7 +55,9 @@ window.addEventListener("keydown", (function(canMove) {
 				$("#container").removeClass("active");
 				$("#games").fadeOut(300);
 				$("#games").removeClass("active");
-				break;
-		}
-	}*/
+				break;		
+		}  
+	};
+	})(true), false);
+
 });
