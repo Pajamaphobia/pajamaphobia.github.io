d9902cc314499cac6a5ffa94806e03942ed966c8
thieve <thieve@users.noreply.github.com>
2017-02-17 16:01:40 -0500

    Add files via upload
    - Moving everything over to Angular instead of JQuery
    - Reads from JSON instead of being a static file

---
+++ app.js
@@ -0,0 +1,81 @@
+/*$(document).ready(function(){
+	$("#menu").bind("mousewheel", function(e){
+		var curScroll = $("#menu").scrollLeft();
+		if(e.originalEvent.wheelDelta > 0) {
+			$("#menu").scrollLeft(curScroll-1936);
+		} else {
+			$("#menu").scrollLeft(curScroll+1936);
+		}
+	});
+
+	$(".scroll").click(function() {
+		var box = $("#menu"), x;
+		if ($(this).hasClass("scrollRight")) {
+			x = (1936) + box.scrollLeft();
+			box.animate({ scrollLeft: x	})
+		} else {
+			x = (1936) - box.scrollLeft();
+			box.animate({ scrollLeft: -x })
+		}
+	});
+
+	$(".system-menu").click(function(){
+		$("#container").toggleClass("active");
+		$("#games").fadeToggle(300);
+		$("#games").toggleClass("active");
+	});
+
+	window.addEventListener("keydown", (function(canMove) {
+	return function(event) {
+		if (!canMove) return false;
+		canMove = false;
+		setTimeout(function() { canMove = true; }, 400);
+		switch (event.keyCode) {
+			case 37:
+				var x = (1936) - $("#menu").scrollLeft();
+				$("#menu").animate({ scrollLeft: -x })
+				break;
+			case 39:
+				var x = (1936) + $("#menu").scrollLeft();
+				$("#menu").animate({ scrollLeft: +x });
+				console.log("new test!");
+				break;
+			case 38:
+				$("#container").toggleClass("active");
+				$("#games").fadeToggle(300);
+				$("#games").toggleClass("active");
+				break;
+			case 40:
+				$("#container").removeClass("active");
+				$("#games").fadeOut(300);
+				$("#games").removeClass("active");
+				break;		
+		}  
+	};
+	})(true), false);
+
+});*/
+
+var App = angular.module('emulationApp', []);
+var gamesShow = false;
+
+App.controller('MainControl', function($scope) {  
+  $scope.selectedIndex = 0;
+  
+  $scope.itemClicked = function ($index) {
+    console.log($index);
+    $scope.selectedIndex = $index;
+  }
+});;
+
+App.controller('SystemsDisplay', function($scope, $http) {
+	$http.get('data/systems.json').then(function(res){
+          $scope.systems = res.data;                
+        });
+});
+
+App.controller('GamesDisplay', function($scope, $http) {
+	$http.get('data/games.json').then(function(res){
+          $scope.systems = res.data;        
+        });
+});
